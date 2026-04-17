5e4bc205157524b92dce9a3d6a5ab76f364f9832
firstaqila <saffana.firsta@gmail.com>
2026-04-17 15:12:50 +0700

    Mount catalog module in main

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,17 @@ use tokio::net::TcpListener;
 use infrastructure::config::AppConfig;
 use infrastructure::database::create_pool;
 use infrastructure::logger::init_tracer;
-use modules::example::infrastructure::create_router;
-use modules::example::infrastructure::AppState;
+use modules::catalog::infrastructure::{create_router, AppState};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     init_tracer();
 
     let config = AppConfig::new().expect("Failed to load configuration from .env file");
+    let pool = create_pool(&config.database_url).await?;
 
-    let _pool = create_pool(&config.database_url).await?;
-
-    let app_state = AppState { _pool };
-
-    let router = create_router(app_state);
+    let state = AppState::new(pool);
+    let router = create_router(state);
 
     let address = format!("{}:{}", config.server_host, config.server_port);
     let listener = TcpListener::bind(&address).await?;
